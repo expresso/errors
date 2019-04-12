87eeef99c799658bfcf330e3e9559ce6fd00fd8a
Lucas Santos <lhs.santoss@gmail.com>
2019-04-12 14:45:51 -0300

    Add capability of propagating user data

--- src/normalizer.ts
+++ src/normalizer.ts
@@ -4,7 +4,8 @@ import { Request, Response, NextFunction, ErrorRequestHandler } from 'express'
 export function factory (): ErrorRequestHandler {
   return (err: Error, _req: Request, _res: Response, next: NextFunction) => {
     if (!(err instanceof Boom)) {
-      return next(Boom.internal(err.message, { stack: err.stack }))
+      const { message, stack } = err
+      return next(Boom.internal(message, { stack: stack, ...err }))
     }
 
     next(err)
