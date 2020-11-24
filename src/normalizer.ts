e6f793fc87d614744c6aa45eac5444305ce85612
Lucas Santos <lhs.santoss@gmail.com>
2020-11-23 21:35:50 -0300

    ⬆️ Upgrade depedencies to latest versions

--- src/normalizer.ts
+++ src/normalizer.ts
@@ -1,9 +1,9 @@
-import Boom from 'boom'
+import Boom from '@hapi/boom'
 import { Request, Response, NextFunction, ErrorRequestHandler } from 'express'
 
 export function factory (): ErrorRequestHandler {
   return (err: Error, _req: Request, _res: Response, next: NextFunction) => {
-    if (!(err instanceof Boom)) {
+    if (!(err instanceof (Boom as any))) {
       const { message, stack } = err
       return next(Boom.internal(message, { stack: stack }))
     }
